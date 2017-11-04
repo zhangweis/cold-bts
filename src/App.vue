f6776bc82857fc950ddecb4b69595430e41feb9a
zhangwei <cdzhangw@gmail.com>
2017-11-05 07:16:33 +0800

    cold-bts

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,18 @@
     <table>
       <tbody>
         <tr>
-          <td>Private Key:</td>
-          <td><input v-model='privateKey' size='80'/></td>
+          <td>Chain Id:</td>
+          <td><input v-model='chainId' size='80'/></td>
         </tr>
         <tr>
-          <td>Transaction JSON:</td>
+          <td>Private Key:</td>
+          <td><input v-model='privateKey' size='80' @keyup='updatePublicKey'/>
+          <br/>
+          {{publicKey}}
+          </td>
+        </tr>
+        <tr>
+          <td>Transaction Buffer(Hex):</td>
           <td>
             <textarea rows="10" cols='80' v-model='trx' @keyup='formatted = formatJson(trx)'>
             </textarea>
@@ -36,41 +43,52 @@
 </template>
 
 <script>
-import {ChainStore, FetchChain, PrivateKey, TransactionHelper, Aes, TransactionBuilder} from "bitsharesjs";
+import {ChainStore, FetchChain, PrivateKey, TransactionHelper, Aes, TransactionBuilder, ops} from "bitsharesjs";
+import {ChainConfig} from "bitsharesjs-ws";
 import {extend} from 'lodash'
 import * as jsonFormat from 'json-format'
 import * as ByteBuffer from 'bytebuffer'
+console.log(ChainConfig)
+ChainConfig.address_prefix = 'BTS'
 export default {
   name: 'app',
-  data () {
+  data: function () {
     return {
       privateKey:'',
-      trx:``      ,
+      publicKey:'',
+      trx:'' ,
       signed:'',
-      formatted:''
+      formatted:'',
+      chainId:'4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8'
     }
   },
   methods: {
-    signIt() {
+    updatePublicKey: function() {
+      var prvKey = PrivateKey.fromWif(this.privateKey)
+      this.publicKey = prvKey.toPublicKey().toString('BTS')
+    },
+    signIt: function() {
       try {
-        var trx1 = JSON.parse(this.trx)
+        // var trx1 = JSON.parse(this.trx)
         var tx = new TransactionBuilder()
-        extend(tx, trx1)
-        tx.tr_buffer = new Buffer(tx.tr_buffer, 'hex')
+        // extend(tx, trx1)
+        var tx = extend(tx, ops.transaction.fromHex(this.trx))
+        tx.tr_buffer = new Buffer(this.trx, 'hex')
         console.log(tx)
         var prvKey = PrivateKey.fromWif(this.privateKey)
         console.log(prvKey)
         tx.add_signer(prvKey)
-        tx.sign(trx1.chain_id)
+        tx.sign(this.chainId)
         this.signed = JSON.stringify(tx.toObject())
       }catch(e) {
         console.error(e)
       }
       // alert(`sign ${this.privateKey}, ${this.trx}`)
     },
-    formatJson(txt) {
-      console.log(jsonFormat(JSON.parse(txt)))
-      return jsonFormat(JSON.parse(txt))
+    formatJson:function (txt) {
+      var obj = ops.transaction.toObject(ops.transaction.fromHex(this.trx))
+      console.log(jsonFormat(obj))
+      return jsonFormat(obj)
     }
   }
 }
