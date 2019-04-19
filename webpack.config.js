77da683587acd5b495e722b79a22e10b0ca76f44
zhangwei <cdzhangw@gmail.com>
2019-04-19 12:36:37 +0800

    appcache

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: MODE_DEV_SERVER?'':'./dist/',
     filename: 'build.js'
   },
   module: {
